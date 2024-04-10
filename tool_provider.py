b6d2c84b099625896e9c949d75c97f1b5af884c6
dhananjaybhandiwad <91888251+dhananjaybhandiwad@users.noreply.github.com>
2024-04-10 13:34:01 +0200

    Minor changes.

--- tool_provider.py
+++ tool_provider.py
@@ -3,11 +3,13 @@ from typing import List
 from langchain.tools import Tool
 
 class BaseToolProvider(ABC):
+    
     @abstractmethod
     def get_tools(self) -> List[Tool]:
         pass
 
 class CustomToolProvider(BaseToolProvider):
+    
     def __init__(self, tools: List[Tool]):
         self.tools = tools
 
